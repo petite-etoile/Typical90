c8794ab288c39acdf26a4519825a2fd1b3e5e9e7
小野隆規 <petite@PetiteEtoile.local>
2021-04-21 17:39:35 +0900

    006

---
+++ 006.cpp
@@ -0,0 +1,97 @@
+/*
+　　　∫ ∫ ∫
+　　　ノヽ
+　　（＿　 ）
+　（＿　　　 ）
+（＿＿＿＿＿＿ ）
+　ヽ(´･ω･)ﾉ　
+　　 |　 /
+　　 UU
+*/
+#pragma region macro
+#include <bits/stdc++.h>
+typedef long long int64;
+using namespace std;
+using P = pair<int64, int64>;
+typedef vector<int> vi;
+const int MOD = (int)1e9 + 7;
+const int64 INF = 1LL << 62;
+const int inf = 1<<30;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i,s,n) for (int i = s; i < (n); i++)
+#define ALL(obj) (obj).begin(), (obj).end() //コンテナじゃないと使えない!!
+#define debug(x) cerr << #x << ": " << x << "\n";
+#define mp make_pair
+#define bn '\n'
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T> &V){
+    int N = V.size();
+    REP(i,N){
+        os << V[i];
+        if (i!=N-1) os << " ";
+    }
+    os << "\n";
+    return os;
+}
+template <typename T,typename S>
+ostream& operator<<(ostream& os, pair<T,S> const&P){
+    os << "(";
+    os << P.first;
+    os << " , ";
+    os << P.second;
+    os << ")";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, set<T> &S){
+    auto it=S.begin();
+    while(it!=S.end()){
+        os << *it;
+        os << " ";
+        it++;
+    }
+    os << "\n";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, deque<T> &q){
+    for(auto it=q.begin();it<q.end();it++){
+        os<<*it;
+        os<<" ";
+    }
+     os<<endl;
+    return os;
+}
+vector<pair<int,int>> dxdy = {mp(0,1),mp(1,0),mp(-1,0),mp(0,-1)};
+#pragma endregion
+//fixed<<setprecision(10)<<ans<<endl;
+
+
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int N, K; string S; cin >> N >> K >> S;
+    
+    string ans(K,'z'+1);
+    int r = 1; //最後の文字の次をさす
+    REP(i,N){
+        int remain = N - i;
+        int l = max(0,K-remain) - 1; //i文字目がl+1文字目(0-indexed)以降にならないとK文字達成できない
+
+        while(r-l > 1){
+            int mid = (l+r)/2;
+            if(ans[mid] > S[i]) r = mid;
+            else l = mid;
+        }
+
+        if(r>=K) continue;
+        else {
+            ans[r++] = S[i];
+        }
+    }
+    cout << ans << endl;
+
+}
