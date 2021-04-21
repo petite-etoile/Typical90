90102000837d9a4d9830ebbb4c535cabdbd8fb29
小野隆規 <petite@PetiteEtoile.local>
2021-04-21 15:50:43 +0900

    001

---
+++ 001.cpp
@@ -0,0 +1,107 @@
+/*
+　　　∫ ∫ ∫
+　　　ノヽ
+　　（＿　 ）
+　（＿　　　 ）
+（＿＿＿＿＿＿ ）
+　ヽ(´･ω･)ﾉ　
+　　 |　 /
+　　 UU
+*/
+#pragma region macro
+#include <bits/stdc++.h>
+typedef long long int64;
+using namespace std;
+using P = pair<int64, int64>;
+typedef vector<int> vi;
+const int MOD = (int)1e9 + 7;
+const int64 INF = 1LL << 62;
+const int inf = 1<<30;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i,s,n) for (int i = s; i < (n); i++)
+#define ALL(obj) (obj).begin(), (obj).end() //コンテナじゃないと使えない!!
+#define debug(x) cerr << #x << ": " << x << "\n";
+#define mp make_pair
+#define bn '\n'
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T> &V){
+    int N = V.size();
+    REP(i,N){
+        os << V[i];
+        if (i!=N-1) os << " ";
+    }
+    os << "\n";
+    return os;
+}
+template <typename T,typename S>
+ostream& operator<<(ostream& os, pair<T,S> const&P){
+    os << "(";
+    os << P.first;
+    os << " , ";
+    os << P.second;
+    os << ")";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, set<T> &S){
+    auto it=S.begin();
+    while(it!=S.end()){
+        os << *it;
+        os << " ";
+        it++;
+    }
+    os << "\n";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, deque<T> &q){
+    for(auto it=q.begin();it<q.end();it++){
+        os<<*it;
+        os<<" ";
+    }
+     os<<endl;
+    return os;
+}
+vector<pair<int,int>> dxdy = {mp(0,1),mp(1,0),mp(-1,0),mp(0,-1)};
+#pragma endregion
+//fixed<<setprecision(10)<<ans<<endl;
+
+
+bool is_ok(int64 leng, vector<int64>const& A, int K){
+    int64 before_cut = 0;
+    int cnt = 0;
+    for(auto a:A){
+        if(a-before_cut >= leng){
+            before_cut = a;
+            cnt++;
+        }
+    }
+    return cnt >= K+1;
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int64 N,L;
+    cin >> N >> L;
+    int64 K; cin >> K;
+    vector<int64> A(N); 
+    REP(i,N) cin >> A[i];
+    A.emplace_back(L);
+
+    
+    int64 ok = 0, ng = L+1;
+    while(ng - ok > 1){
+        int64 mid = (ok+ng)/2;
+        if(is_ok(mid,A,K)) ok = mid;
+        else ng = mid;
+    }
+
+    int64 ans=ok;
+
+
+
+    cout << ans << endl;
+}
